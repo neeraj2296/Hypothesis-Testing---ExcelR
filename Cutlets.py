b58372547c4810ad841d61438d9e0b5ec11595ed
neeraj2296 <62831612+neeraj2296@users.noreply.github.com>
2020-04-13 20:35:10 +0530

    Update Cutlets.py

--- Cutlets.py
+++ Cutlets.py
@@ -2,14 +2,14 @@
 """
 Created on Fri Apr  3 23:49:34 2020
 
-@author: Admin
+@author: Neeraj Kumar S J
 """
-
+########################################importing the necassary modules###############################################
 import scipy
 from scipy import stats
 import statsmodels.api as sm
 import pandas as pd
-
+#######################################Importing the Cutlets.csv dataset #############################################
 cutlet_size = pd.read_csv("E:\\Neeraj\\Exam and Careers\\DataScience\\Data Sets\\Cutlets.csv")
 cutlet_size.columns = 'UnitA','UnitB' #Inordeer to rename the columns
 #Normality Test.
@@ -25,7 +25,7 @@ print(f'As P Value for Unit B or Sample Y1 :{P_value_unitB} is greater than 0.05
 #Variance Test
 z = scipy.stats.levene(cutlet_size.UnitA,cutlet_size.UnitB)
 print(f'As P Value for comparing Unit A or Sample Y1 and UnitB or Sample Y2 :{z[1]} is greater than 0.05, Hence we can conclude Variances of Sample Y1 and Y2 are same ')
-
+#################################################Printng the obained results###############################################
 t_test = scipy.stats.ttest_ind(cutlet_size.UnitA,cutlet_size.UnitB)
 print(f'As P Value for t test on Unit A or Sample Y1 and UnitB or Sample Y2 :{t_test[1]} is greater than 0.05')
 
